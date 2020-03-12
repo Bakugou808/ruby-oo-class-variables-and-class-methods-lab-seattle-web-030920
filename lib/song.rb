b5469a1f7fa6eb03d98dfb87919087305bb04747
Bakugou808 <janu.m.sung@gmail.com>
2020-03-11 21:12:48 -0700

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,61 @@
+require 'pry'
+
+class Song
+
+    @@count = 0 
+    @@genres = []
+    @@artists = []
+
+    attr_accessor :name, :artist, :genre
+    
+
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+
+        @@count += 1
+        @@genres << genre 
+        @@artists << artist 
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.artists
+        @@artists.uniq 
+    end
+
+    def self.genre_count
+        genre_hash = {}
+        @@genres.map {|genre| 
+            if genre_hash[genre] 
+                genre_hash[genre] += 1
+            else
+                genre_hash[genre] = 1
+            end
+        }
+        genre_hash 
+    end
+
+    def self.artist_count 
+        artist_hash = {}
+        @@artists.map {|artist|
+            if artist_hash[artist]
+                artist_hash[artist] += 1
+            else
+                artist_hash[artist] = 1
+            end
+        }
+        artist_hash
+    end 
+end
+# test1 = Song.new("Jigga Who?", "jay", "rap")
+# test2 = Song.new("Who that girl?", "Neptunes", "rap")
+# binding.pry
